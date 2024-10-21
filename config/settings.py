dee9dfa31dbd11fab74129dcd28d20874cf36898
김성범 <kimseongbeom@gimseongbeom-ui-MacBookPro.local>
2024-10-21 21:29:28 +0900

    Models and Migrations

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,13 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+CUSTOM_APPS = [
+    'houses.apps.HousesConfig',
+
+]
+
+
+SYSTEM_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+INSTALLED_APPS = SYSTEM_APPS + CUSTOM_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
