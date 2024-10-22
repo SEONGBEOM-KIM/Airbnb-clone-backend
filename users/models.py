beef118b889b495f45cc0b0c5e85a2824736846a
김성범 <kimseongbeom@gimseongbeom-ui-MacBookPro.local>
2024-10-22 14:45:52 +0900

    Custom Fields

--- users/models.py
+++ users/models.py
@@ -3,4 +3,11 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+
+    # AbstractUser first_name, last_name overriding
+    first_name = models.CharField(
+        max_length=150, editable=False
+    )  # editable=False -> user cannot use
+    last_name = models.CharField(max_length=150, editable=False)
+    name = models.CharField(max_length=150, default="")
+    is_host = models.BooleanField(default=False)
