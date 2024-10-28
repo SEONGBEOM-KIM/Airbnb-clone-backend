73e1b8584c96846ecb291834b1b738d77f92ff81
김성범 <love29ana@gmail.com>
2024-10-28 15:29:26 +0900

    Admin Methods

--- rooms/admin.py
+++ rooms/admin.py
@@ -9,8 +9,8 @@ class RoomAdmin(admin.ModelAdmin):
         "price",
         "kind",
         "owner",
+        "total_amenities",
         "created_at",
-        "updated_at",
     )
 
     list_filter = (
@@ -20,6 +20,9 @@ class RoomAdmin(admin.ModelAdmin):
         "kind",
         "amenities",
     )
+
+    def total_amenities(self, room):
+        return room.amenities.count()
 
 
 @admin.register(Amenity)
