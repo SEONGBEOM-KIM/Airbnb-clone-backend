52425b0a30379f6aa6bc08406079e1e8e8ec64bb
김성범 <love29ana@gmail.com>
2024-10-31 21:49:02 +0900

    ModelSerializer

--- categories/serializers.py
+++ categories/serializers.py
@@ -2,29 +2,8 @@ from rest_framework import serializers
 from .models import Category
 
 
-class CategorySerializer(serializers.Serializer):
+class CategorySerializer(serializers.ModelSerializer):
 
-    # Explaining to Serializer: what model have, what type it is
-
-    pk = serializers.IntegerField(
-        read_only=True,
-    )
-    name = serializers.CharField(
-        required=True,
-        max_length=50,
-    )
-    kind = serializers.ChoiceField(
-        choices=Category.CategoryKindChoices.choices,
-    )
-    created_at = serializers.DateTimeField(
-        read_only=True,
-    )
-
-    def create(self, validated_data):  # <- python object
-        return Category.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get("name", instance.name)
-        instance.kind = validated_data.get("kind", instance.kind)
-        instance.save()
-        return instance
+    class Meta:
+        model = Category  # give a model to serializer
+        fields = "__all__"  # fields = ("name", "kind",) or exclude = ("created_at",)
