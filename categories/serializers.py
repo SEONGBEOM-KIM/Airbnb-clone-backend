bfff2be248896f0b4b847c4d6ae3c57976863545
김성범 <love29ana@gmail.com>
2024-10-31 15:07:50 +0900

    POST Requests

--- categories/serializers.py
+++ categories/serializers.py
@@ -5,6 +5,7 @@ class CategorySerializer(serializers.Serializer):
 
     # Explaining to Serializer: what model have, what type it is
 
+    pk = serializers.IntegerField()
     name = serializers.CharField(required=True)
     kind = serializers.CharField()
     created_at = serializers.DateTimeField()
