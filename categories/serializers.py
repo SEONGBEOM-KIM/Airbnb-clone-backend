416db4e5b02cfa3d24356b492f09d670e611b3fe
김성범 <love29ana@gmail.com>
2024-10-31 15:19:01 +0900

    is_valid()

--- categories/serializers.py
+++ categories/serializers.py
@@ -5,7 +5,16 @@ class CategorySerializer(serializers.Serializer):
 
     # Explaining to Serializer: what model have, what type it is
 
-    pk = serializers.IntegerField()
-    name = serializers.CharField(required=True)
-    kind = serializers.CharField()
-    created_at = serializers.DateTimeField()
+    pk = serializers.IntegerField(
+        read_only=True,
+    )
+    name = serializers.CharField(
+        required=True,
+        max_length=50,
+    )
+    kind = serializers.CharField(
+        max_length=15,
+    )
+    created_at = serializers.DateTimeField(
+        read_only=True,
+    )
