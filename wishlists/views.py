573ea60b3eb0a5f670a41d311965fb669e0b083e
김성범 <love29ana@gmail.com>
2024-11-05 22:54:18 +0900

    Wishlists

--- wishlists/views.py
+++ wishlists/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import Wishlist
+from .serializers import WishlistsSerializer
 
-# Create your views here.
+
+class Wishlists(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        all_wishlists = Wishlist.objects.filter(user=request.user)
+        serializer = WishlistsSerializer(
+            all_wishlists,
+            many=True,
+            context={"request": request},
+        )
+        return Response(serializer.data)
+
+    def post(self, request):
+        serilalizer = WishlistsSerializer(data=request.data)
+        if serilalizer.is_valid():
+            wishlist = serilalizer.save(user=request.user)
+            serilalizer = WishlistsSerializer(wishlist)
+            return Response(serilalizer.data)
+        else:
+            return Response(serilalizer.errors)
