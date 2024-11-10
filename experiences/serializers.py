b87b1c5b3444d0cd5cf3117e1586fd1f592669cc
김성범 <love29ana@gmail.com>
2024-11-10 22:48:02 +0900

    Custom Authentication

--- experiences/serializers.py
+++ experiences/serializers.py
@@ -1,8 +1,31 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Perk
+from users.serializers import TinyUserSerializer
+from categories.serializers import CategorySerializer
+from .models import Perk, Experience
 
 
 class PerkSerializer(ModelSerializer):
     class Meta:
         model = Perk
-        fields = "__all__"
+        fields = (
+            "name",
+            "detail",
+            "explanation",
+        )
+
+
+class ExperienceSerializer(ModelSerializer):
+
+    host = TinyUserSerializer(read_only=True)
+    category = CategorySerializer(read_only=True)
+    perks = PerkSerializer(
+        read_only=True,
+        many=True,
+    )
+
+    class Meta:
+        model = Experience
+        exclude = (
+            "created_at",
+            "updated_at",
+        )
--- experiences/views.py
+++ experiences/views.py
@@ -1,9 +1,12 @@
+from django.db import transaction
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.exceptions import NotFound
+from rest_framework.exceptions import NotFound, ParseError
 from rest_framework.status import HTTP_204_NO_CONTENT
-from .serializers import PerkSerializer
-from .models import Perk
+from rest_framework.permissions import IsAuthenticated
+from categories.models import Category
+from .serializers import PerkSerializer, ExperienceSerializer
+from .models import Perk, Experience
 
 
 class Perks(APIView):
@@ -55,3 +58,48 @@ class PerkDetail(APIView):
         perk = self.get_object(pk)
         perk.delete()
         return Response(status=HTTP_204_NO_CONTENT)
+
+
+class Experiences(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        all_experiences = Experience.objects.all()
+        serializer = ExperienceSerializer(
+            all_experiences,
+            many=True,
+        )
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ExperienceSerializer(data=request.data)
+        if serializer.is_valid():
+            category_pk = request.data.get("category")
+            if not category_pk:  # <- category not in request
+                raise ParseError("Category is required")
+            try:  # <- have category in request
+                category = Category.objects.get(pk=category_pk)
+                if category.kind == Category.CategoryKindChoices.ROOMS:
+                    raise ParseError("The Category kind should be 'experience'")
+            except Category.DoesNotExist:  # <- user give wrong category with db
+                raise ParseError("Category not found")
+            try:
+                with transaction.atomic():
+                    experience = serializer.save(
+                        host=request.user,
+                        category=category,
+                    )
+                    perks = request.data.get("perks")
+                    for perk_pk in perks:
+                        perk = Perk.objects.get(pk=perk_pk)
+                        experience.perks.add(perk)
+                    serializer = ExperienceSerializer(
+                        experience,
+                        context={"request": request},
+                    )
+                    return Response(serializer.data)
+            except Exception:
+                raise ParseError("Perk not found")
+        else:
+            return Response(serializer.errors)
